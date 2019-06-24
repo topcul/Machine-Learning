e5c21a12bdbef291c85ad8a867df5d070f2b0a3f
topcul <41707803+topcul@users.noreply.github.com>
2019-06-25 01:36:05 +0300

    Add files via upload

---
+++ Activity3.java
@@ -0,0 +1,16 @@
+package com.example.sr.uygulama1;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by SR on 28.2.2018.
+ */
+
+public class Activity3 extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity3);//Bu java dosyasının ilişkilendirilmiş layout dosyası activity3
+    }
+}
